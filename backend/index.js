5558ab6a8d66ca5c6630cee825c5a311b1d2b199
ahmed Dasso <65762595+DassoAhmed@users.noreply.github.com>
2024-12-09 11:07:51 +0000

    Set-up Database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from 'express'
+import mongoose  from 'mongoose'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import routes from './routes/soccerRoutes'
 
 const app = express();
 const PORT = 4000;
 
+// mongo connection
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/soccerDB')
+
+// bodyparser
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+//Cors
+app.use(cors())
+
+routes(app)
 
 app.get('/',(req, res) =>
     res.send(`Our Application Is Running ${PORT}` )
