f73fa899bff0153edf40e07238c87630eb810511
ahmed Dasso <65762595+DassoAhmed@users.noreply.github.com>
2024-12-14 11:01:59 +0000

    create POST endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import mongoose  from 'mongoose'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import routes from './routes/soccerRoutes'
+import express from 'express';
+import bodyparser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import routes from './routes/soccerRoutes';
 
 const app = express();
 const PORT = 4000;
@@ -12,8 +12,8 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/soccerDB')
 
 // bodyparser
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(bodyparser.urlencoded({ extended: true}))
+app.use(bodyparser.json())
 
 //Cors
 app.use(cors())
